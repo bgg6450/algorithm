3e0e66f9eefee53b0f4cfce92835e4fe90756dac
dns <dnsdevelop>
2022-12-28 13:52:35 +0900

    최소 직사각형

---
+++ src/level1/Solution2.java
@@ -0,0 +1,14 @@
+package level1;
+
+// 문자열 나누기
+public class Solution2 {
+    public int solution(String s) {
+        int answer = 0;
+        int a = 0;
+        int b = 0;
+        for (int i = 0; i < s.length(); i++) {
+            a++;
+        }
+        return answer;
+    }
+}
