020e5a6b6105777a06927da078aa1d0a3ad007d7
gyeonggeon <bgg6450@gmail.com>
2023-01-02 00:20:50 +0900

    1845 solved

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,3 @@
-import level1.Solution;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Hello {
     public static void main(String[] args) {
     }
--- src/level1/Solution.java
+++ src/level1/Solution.java
@@ -1,18 +1,13 @@
 package level1;
 
-// 삼총사
+import java.util.HashSet;
+
 public class Solution {
-    public int solution(int[] number) {
-        int answer = 0;
-        for (int i = 0; i < number.length-2; i++) {
-            for (int j = i+1; j < number.length-1; j++) {
-                for (int k = j+1; k < number.length; k++) {
-                    if (number[i] + number[j] + number[k] == 0) {
-                        answer++;
-                    }
-                }
-            }
+    public int solution(int[] nums) {
+        HashSet<Integer> a = new HashSet<>();
+        for (int i : nums) {
+            a.add(i);
         }
-        return answer;
+        return Math.min(nums.length/2, a.size());
     }
 }
