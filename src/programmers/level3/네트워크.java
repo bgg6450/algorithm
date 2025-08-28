bd8e261fde661401c775782a7dde0629e550b962
geon.baek <geon.baek@kt.com>
2025-08-28 12:59:15 +0900

    [Programmers] lv3.네트워크

---
+++ src/programmers/level3/네트워크.java
@@ -0,0 +1,57 @@
+package programmers.level3;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class 네트워크 {
+
+    public int solution_dfs(int n, int[][] computers) {
+        int result = 0;
+        boolean[] visited = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i, visited, computers);
+                result++;
+            }
+        }
+        return result;
+    }
+
+    private void dfs(int i, boolean[] visited, int[][] computers) {
+        visited[i] = true;
+        for (int j = 0; j < computers.length; j++) {
+            if (!visited[j] && computers[i][j] == 1) {
+                dfs(j, visited, computers);
+            }
+        }
+    }
+
+    public int solution_bfs(int n, int[][] computers) {
+        int result = 0;
+        boolean[] visited = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                bfs(i, n, computers, visited);
+                result++;
+            }
+        }
+        return result;
+    }
+
+    private void bfs(int start, int n, int[][] computers, boolean[] visited) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        visited[start] = true;
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            for (int i = 0; i < n; i++) {
+                if (!visited[i] && computers[current][i] == 1) {
+                    visited[i] = true;
+                    queue.add(i);
+                }
+            }
+        }
+    }
+}
