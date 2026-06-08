328a8cd3404a26efcaa122e5b5c3edad76def2e7
geon.baek <geon.baek@kt.com>
2026-06-08 13:00:58 +0900

    Programmers: 행렬 테두리 회전하기 - 풀이 중

---
+++ src/programmers/level2/행렬_테두리_회전하기.java
@@ -0,0 +1,14 @@
+package programmers.level2;
+
+/*
+rows	columns	    queries	                                        result
+6	    6	        [[2,2,5,4],[3,3,6,6],[5,1,6,3]]	[8, 10, 25]
+3	    3	        [[1,1,2,2],[1,2,2,3],[2,1,3,2],[2,2,3,3]]	    [1, 1, 5, 3]
+100	    97	        [[1,1,100,97]]	                                [1]
+ */
+public class 행렬_테두리_회전하기 {
+    public int[] solution(int rows, int columns, int[][] queries) {
+        int[] answer = {};
+        return answer;
+    }
+}
